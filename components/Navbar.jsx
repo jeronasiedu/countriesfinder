ee75213777a2a8d621671a304fe1141d08ae0dec
Jeron <norejudeisa@gmail.com>
2022-04-01 22:28:59 +0000

    fixed padding issues

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const icon = colorMode === 'light' ? <BiMoon /> : <BiSun />
   const bg = colorMode === 'light' ? '#fff' : 'hsl(207, 26%, 17%)'
   return (
-    <HStack bg={bg} py={3} px={[2, 4, 6, 10]} shadow="md" as="nav" mb={8}>
+    <HStack bg={bg} py={3} px={[4, 6, 10]} shadow="md" as="nav" mb={8}>
       <Heading size="md">Where in the world?</Heading>
       <Spacer />
       <IconButton icon={icon} onClick={toggleColorMode} />
